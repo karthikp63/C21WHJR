3fc1fb2c24a568614893dc5cac85f072180d91ee
karthikp63 <73623494+karthikp63@users.noreply.github.com>
2020-11-11 17:59:45 -0800

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,47 @@
+var fixedRect, movingRect;
+var car, wall;
+function setup() {
+  createCanvas(1200,800);
+  fixedRect = createSprite(600, 400, 50, 80);
+  fixedRect.shapeColor = "green";
+  fixedRect.debug = true;
+  movingRect = createSprite(400,200,80,30);
+  movingRect.shapeColor = "green";
+  movingRect.debug = true;
+  car = createSprite(40,400, 40,40)
+  car.shapeColor = "blue";
+  car.velocityX = 4;
+  wall = createSprite(1000,400, 40,100)
+  wall.shapeColor = "orange";
+}
+
+function draw() {
+  background(0,0,0);  
+  movingRect.x = World.mouseX;
+  movingRect.y = World.mouseY;
+  if (isTouching(car, wall)){
+    car.shapeColor = "red";
+  }
+  collide(car, wall);
+  drawSprites();
+}
+
+function isTouching(object1, object2){
+  if (object1.x - object2.x < object2.width/2 + object1.width/2
+    && object2.x - object1.x < object2.width/2 + object1.width/2
+    && object1.y - object2.y < object2.height/2 + object1.height/2
+    && object2.y - object1.y < object2.height/2 + object1.height/2) {
+      return true;
+  } else {
+    return false;
+  }
+}
+function collide(object1, object2, ){
+  if(object1.x - object2.x < object2.width/2 + object1.width/2
+    && object2.x - object1.x < object2.width/2 + object1.width/2
+    && object1.y - object2.y < object2.height/2 + object1.height/2
+    && object2.y - object1.y < object2.height/2 + object1.height/2){
+      object1.setVelocity(0,0);
+      object2.setVelocity(0,0);
+    }
+}
